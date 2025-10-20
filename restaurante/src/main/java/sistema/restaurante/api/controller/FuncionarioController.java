67d3225178cc85f5dcda25837e00b422186fc448
Natan William <natanwill.contatar@outlook.com>
2025-10-20 20:56:10 -0300

    Funcionario

--- restaurante/src/main/java/sistema/restaurante/api/controller/FuncionarioController.java
+++ restaurante/src/main/java/sistema/restaurante/api/controller/FuncionarioController.java
@@ -1,14 +1,15 @@
 package sistema.restaurante.api.controller;
 
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sistema.restaurante.api.dto.FuncionarioDTORequest;
+import sistema.restaurante.api.dto.FuncionarioDTOResponse;
 import sistema.restaurante.application.service.FuncionarioService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/funcionarios")
@@ -20,10 +21,16 @@ public class FuncionarioController {
         this.funcionarioService = funcionarioService;
     }
 
-    @PostMapping()
-    public ResponseEntity cadastrarFuncionario(@Valid @RequestBody FuncionarioDTORequest dto) {
+    @PostMapping
+    public ResponseEntity<FuncionarioDTOResponse> cadastrarFuncionario(@Valid @RequestBody FuncionarioDTORequest dto) {
         var funcionario = funcionarioService.cadastrarFuncionario(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(funcionario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FuncionarioDTOResponse>> listarFuncionarios(Pageable pageable) {
+        var funcionarios = funcionarioService.listarFuncionarios(pageable).getContent();
+        return ResponseEntity.status(HttpStatus.CREATED).body(funcionarios);
     }
 
 }
